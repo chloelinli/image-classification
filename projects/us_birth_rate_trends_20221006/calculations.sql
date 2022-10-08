09b1d4cdf71b021c13af26dca14b196023029527
chloe <chloe.rushing@outlook.com>
2022-10-07 20:45:19 -0700

    created template for queries

--- projects/us_birth_rate_trends_20221006/calculations.sql
+++
@@ -1,5 +0,0 @@
-/*
-Using Google BigQuery to run queries;
-created a dataset named us_birth_rate_trends
-and uploaded each csv file as a table
-*/
---
+++ projects/us_birth_rate_trends_20221006/queries/20_to_24_differences.sql
@@ -0,0 +1,38 @@
+/*
+These queries use BigQuery to calculate the difference in values within the
+birth rates for each race within the 20-24 age group. These values were
+then manually written into a Google Spreadsheet and formatted as percentages.
+"All" refers to the section labeled "All races" as provided by the CDC dataset.
+Unsure what "Single Race" is meant to refer to, but will continue to calculate
+the data separately and include it in the spreadsheets.
+*/
+
+-- All races
+
+-- Asian, not Hispanic or Latina (Single Race)
+
+-- Asian, Pacific Islander
+
+-- Asian, Pacific Islander, not Hispanic or Latina
+
+-- Black, African American
+
+-- Black, African American (Single Race)
+
+-- Black, African American, not Hispanic or Latina
+
+-- Black, African American, not Hispanic or Latina (Single Race)
+
+-- Hispanic or Latina
+
+-- American Indian, Alaska Native
+
+-- American Indian, Alaska Native, not Hispanic or Latina
+
+-- American Indian, Alaska Native, not Hispanic or Latina (Single Race)
+
+-- White
+
+-- White, not Hispanic or Latina
+
+-- White, not Hispanic or Latina (Single Race)
