45613b7906a41e82d5c24c4ac045557f78f4ad6d
chloe <chloe.rushing@outlook.com>
2022-10-07 21:04:29 -0700

    20-24 differences

--- projects/us_birth_rate_trends_20221006/queries/20_to_24_differences.sql
+++ projects/us_birth_rate_trends_20221006/queries/20_to_24_differences.sql
@@ -11,90 +11,90 @@ include it in the spreadsheets.
 
 -- All races
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_all`
 ORDER BY Year;
 
 -- Asian, not Hispanic or Latina (Single Race)
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_asian_not_hl_single_race`
 ORDER BY Year;
 
 -- Asian, Pacific Islander
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_asian_pi`
 ORDER BY Year;
 
 -- Asian, Pacific Islander, not Hispanic or Latina
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_asian_pi_not_hl`
 ORDER BY Year;
 
 -- Black, African American
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_black`
 ORDER BY Year;
 
 -- Black, African American (Single Race)
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_black_single_race`
 ORDER BY Year;
 
 -- Black, African American, not Hispanic or Latina
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_black_not_hl`
 ORDER BY Year;
 
 -- Black, African American, not Hispanic or Latina (Single Race)
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_black_not_hl_single_race`
 ORDER BY Year;
 
 -- Hispanic or Latina
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_hispanic_latina`
 ORDER BY Year;
 
 -- American Indian, Alaska Native
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_native`
 ORDER BY Year;
 
 -- American Indian, Alaska Native, not Hispanic or Latina
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_native_not_hl`
 ORDER BY Year;
 
 -- American Indian, Alaska Native, not Hispanic or Latina (Single Race)
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_native_not_hl_single_race`
 ORDER BY Year;
 
 -- White
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_white`
 ORDER BY Year;
 
 -- White, not Hispanic or Latina
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_white_not_hl`
 ORDER BY Year;
 
 -- White, not Hispanic or Latina (Single Race)
 SELECT
-  _15_19_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
+  _20_24_years__percentage_ - LAG(_15_19_years__percentage_) OVER (ORDER BY Year) AS diff_curr_prev_percentage
 FROM `myportfolio-110818.us_birth_rate_trends.birth_rates_white_not_hl_single_race`
 ORDER BY Year;
