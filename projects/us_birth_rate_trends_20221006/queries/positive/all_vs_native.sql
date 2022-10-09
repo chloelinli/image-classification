fe30faf8c13b64df42183bcf95e48fa2e5008f6a
chloe <chloe.rushing@outlook.com>
2022-10-08 21:59:00 -0700

    template for positive queries

---
+++ projects/us_birth_rate_trends_20221006/queries/positive/all_vs_native.sql
@@ -0,0 +1,21 @@
+/*
+These queries output all positive percentage differences,
+comparing "all races" with American Indian or Alaskan Native
+races in the U.S. by age.
+*/
+
+-- 15 to 17
+
+-- 15 to 19
+
+-- 18 to 19
+
+-- 20 to 24
+
+-- 25 to 29
+
+-- 30 to 34
+
+-- 35 to 39
+
+-- 40 to 44
