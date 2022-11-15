f4537fb63271dd680f50602ed1ea2f592a31e213
chloe <chloe.rushing@outlook.com>
2022-11-15 14:13:39 -0800

    queried official data, removed unofficial

--- projects/spotify_listening/data/official/official_queries_song_count.sql
+++ projects/spotify_listening/data/official/official_queries_song_count.sql
@@ -1,8 +1,35 @@
--- total times listened to a song in the past year
+/*
+Queries made using person BigQuery project named myportfolio-110818 under dataset spotify_trends.
+
+Tables used:
+official_1_year for all official personal Spotify stats for past year
+official_1_year_genre_langage from first query, with added genre and language columns
+*/
+
+
+-- 1. total times listened to a song in the past year, limited to top 50 excluding podcasts
 SELECT
   Title,
   Artist,
   COUNT(Title) AS Times_Listened
 FROM `myportfolio-110818.spotify_trends.official_1_year`
+WHERE ARTIST != 'Rotten Mango'
 GROUP BY Title, Artist
-ORDER BY Times_Listened DESC;
+ORDER BY Times_Listened DESC
+LIMIT 50;
+
+-- 2. grouping total listenings for each language
+SELECT
+  Language,
+  COUNT(Times_Listened) AS Total_Language_Listened
+FROM `myportfolio-110818.spotify_trends.official_1_year_genre_language`
+GROUP BY Language
+ORDER BY Total_Language_Listened DESC;
+
+-- 3. grouping total listenings for each genre
+SELECT
+  Genre,
+  COUNT(Times_Listened) AS Total_Genre_Listened
+FROM `myportfolio-110818.spotify_trends.official_1_year_genre_language`
+GROUP BY Genre
+ORDER BY Total_Genre_Listened DESC;
