53ee31e0cf0ce96b3b792fc7d6e26ed9d3cbaabb
chloe <chloe.rushing@outlook.com>
2022-11-15 08:32:19 -0800

    renamed vars for comprehension

--- projects/spotify_listening/data/official/json_to_csv.py
+++ projects/spotify_listening/data/official/json_to_csv.py
@@ -3,16 +3,16 @@ import csv
 import json
 
 # streaming history
-input = 'projects\spotify_listening\data\official\StreamingHistory0.json'
-output = 'projects\spotify_listening\data\official\official_stats_1_year.csv'
+json_data = 'projects\spotify_listening\data\official\StreamingHistory0.json'
+csv_conv = 'projects\spotify_listening\data\official\official_stats_1_year.csv'
 
-data = open(input, encoding='utf8')
-new_csv = open(output, 'w', encoding='utf8')
-loaded_data = json.load(data)
-data.close()
+input = open(json_data, encoding='utf8')
+output = open(csv_conv, 'w', encoding='utf8')
+data = json.load(input)
+input.close()
 
-out1 = csv.writer(new_csv)
+new_csv = csv.writer(output)
 
-out1.writerow(loaded_data[0].keys()) 
-for r in loaded_data:
-    out1.writerow(r.values())
+new_csv.writerow(data[0].keys()) 
+for r in data:
+    new_csv.writerow(r.values())
