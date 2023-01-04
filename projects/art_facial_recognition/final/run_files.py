f4e4907bd4373a36222b2f7baa0518b02dfa52b1
chloe <chloe.rushing@outlook.com>
2023-01-03 17:51:41 -0800

    added second conv file

--- projects/art_facial_recognition/final/run_files.py
+++ projects/art_facial_recognition/final/run_files.py
@@ -2,6 +2,7 @@ import os
 
 # image conversion
 os.system('python3 projects/art_facial_recognition/final/image_to_csv.py')
+os.system('python3 projects/art_facial_recognition/final/image_to_csv_MH.py')
 
 # image reconstruction
 os.system('python3 projects/art_facial_recognition/final/reconstruct_images.py')
