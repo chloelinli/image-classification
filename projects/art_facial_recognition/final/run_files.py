58501875fb366fd39e1099840108bc5817f41b2b
chloe <chloe.rushing@outlook.com>
2023-01-05 16:14:21 -0800

    added recognize images to run_files

--- projects/art_facial_recognition/final/run_files.py
+++ projects/art_facial_recognition/final/run_files.py
@@ -7,3 +7,6 @@ os.system('python3 projects/art_facial_recognition/final/image_to_csv_H.py')
 
 # image reconstruction
 os.system('python3 projects/art_facial_recognition/final/reconstruct_images.py')
+
+# image recognition
+os.system('python3 projects/art_facial_recognition/final/recognize_images.py')
