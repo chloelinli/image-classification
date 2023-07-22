87baae78f2d8e9fddf945f5abb030eee69cad6a9
chloe <chloe.rushing@outlook.com>
2023-07-22 13:09:53 -0700

    split date column

--- projects/sales_data_analysis/data/analysis.py
+++ projects/sales_data_analysis/data/analysis.py
@@ -3,7 +3,15 @@ import pandas as pd
 import numpy as np
 
 def main():
-    print()
+    path = 'projects/sales_data_analysis/data'
+    df = pd.read_csv(path+'/Sales_Transaction_reformatted.csv')
+
+    # split Date into Year, Month, and Day columns
+    temp = df['Date'].str.split("-", expand=True).astype('int')
+    temp.columns = ['Year', 'Month', 'Day']
+    df.insert(2, 'Year', temp['Year'])
+    df.insert(3, 'Month', temp['Month'])
+    df.insert(4, 'Day', temp['Day'])
 
 if __name__ == '__main__':
     main()
