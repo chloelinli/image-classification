d0cd17838dcbd7e1ce686facb2d81357c431e933
chloe <chloe.rushing@outlook.com>
2023-07-27 10:31:35 -0700

    added new year-month column as string

--- projects/sales_data_analysis/data/analysis.py
+++ projects/sales_data_analysis/data/analysis.py
@@ -1,5 +1,7 @@
 # import statements
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
     path = 'projects/sales_data_analysis/data'
@@ -45,10 +47,12 @@ def transforming(data):
     # split Date into Year, Month, and Day columns
     index = data.columns.get_loc('Date')
     temp = data['Date'].str.split("-", expand=True).astype('int')
-    temp.columns = ['Year', 'Month', 'Day']
+    temp1 = data['Date'].str.split("-", expand=True)
+    temp.columns = temp1.columns = ['Year', 'Month', 'Day']
     data.insert(index+1, 'Year', temp['Year'])
     data.insert(index+2, 'Month', temp['Month'])
     data.insert(index+3, 'Day', temp['Day'])
+    data.insert(index+4, 'Year-Month', temp1['Year']+'-'+temp1['Month'])
 
     # add new column for items actually bought -> canceled items as 0
     index = data.columns.get_loc('Quantity')
@@ -60,7 +64,6 @@ def transforming(data):
     # add column for quantity x price
     index = data.columns.get_loc('QuantitySold')
     data.insert(index+1, 'FinalPrice', data['Price']*data['QuantitySold'])
-    print(data)
     
     return data
 
@@ -71,10 +74,17 @@ questions:
 """
 def analysis(data):
 
-    """group transactions - TransactionNo/total lines"""
     # 23204 total transactions, 19790 completed transactions
     #   3414 canceled, math and code checks out -> unneeded now
     noncanceled = data[data['QuantitySold'] > 0]
+
+    
+    """product quantity and sales - ProductNo/Quantity/Date"""
+    """time of purchases - total lines/Date/ProductNo"""
+    productSales(noncanceled)
+    
+
+    """group transactions - TransactionNo/total lines"""
     transac = noncanceled.groupby(['TransactionNo'])
 
     # total number of transactions
@@ -82,18 +92,27 @@ def analysis(data):
 
     # total spent per transaction
     # what is the total number of sales?
-    transacSpent = transac.aggregate({
-        'Price':'sum','QuantitySold':'sum'}).reset_index()
+    transacSpent = transac.aggregate({'Price':'sum','QuantitySold':'sum'}).reset_index()
     #print(transacSpent)
 
 
-    """time of purchases - total lines/Date/ProductNo"""
-    # what is the average sales per month?
-    # time of month/year of most purchases
-    # sales per item per month?
+    """customer stats - CustomerNo/ProductNo/Quantity"""
+    customers = noncanceled.groupby(['CustomerNo'])
+
+    # total orders (made in a year)?
+    #print(customers.aggregate({'TransactionNo':'count'}))
+
+    # number of times purchasing an item
+    customerProduct = noncanceled.groupby(['CustomerNo', 'ProductNo'])
+    #print(customerProduct.aggregate({'ProductNo':'count'}))
 
 
-    """product quantity and sales - ProductNo/Quantity/Date"""
+"""
+this method contains the analysis of product sales
+focusing on time of month/year and total sales"""
+def productSales(data):
+    x = 1
+    # group and aggregate data
     products = data.groupby(['ProductNo'])
 
     # which products sell best?
@@ -104,16 +123,40 @@ def analysis(data):
     
     # total sales/quantity sold in a year
 
+    yearMonth = data.groupby(['Year', 'Month'])
+    sales = data.groupby(['Year-Month', 'ProductNo'])
 
-    """customer stats - CustomerNo/ProductNo/Quantity"""
-    customers = data.groupby(['CustomerNo'])
+    # what is the average sales per month?
+    avgSales = yearMonth.aggregate({'FinalPrice':'mean'})
+    #print(avgSales['FinalPrice'].round(decimals=2))
+    
+    # time of month/year of most purchases
+    # sales per item per month?
+    monthlySales = sales.aggregate({'Quantity':'sum'}).reset_index()
+    print(monthlySales)
+    # graph
 
-    # total orders (made in a year)?
-    #print(customers.aggregate({'TransactionNo':'count'}))
+    """
+    # graph month vs total quantity of products sold
+    year2018 = monthlySales[monthlySales['Year'] == 2018]
+    year2019 = monthlySales[monthlySales['Year'] == 2019]
+    graph = sns.FacetGrid(year2019, col='ProductNo', row='Month')
+    graph.map(sns.scatterplot, 'Quantity')
+    plt.show()
+    """
 
-    # number of times purchasing an item
-    customerProduct = data.groupby(['CustomerNo', 'ProductNo'])
-    #print(customerProduct.aggregate({'ProductNo':'count'}))
+
+def m2():
+    y = 2
+    # group and aggregate data
+
+    # graph
+
+def m3():
+    z = 3
+    # group and aggregate data
+
+    # graph
 
 if __name__ == '__main__':
     main()
