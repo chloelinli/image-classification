ae89cba219d1aa4a9df2218320b6b78d1511e338
chloe <chloe.rushing@outlook.com>
2025-02-16 14:51:42 -0800

    adjust plot for initial char, add subplot function for unrel char

--- projects/art_facial_recognition/final/graph_accuracy.py
+++ projects/art_facial_recognition/final/graph_accuracy.py
@@ -3,10 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-"""
-format multi bar chart
-https://matplotlib.org/stable/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
-"""
 
 def main():
 
@@ -16,8 +12,11 @@ def main():
     [init_methods, init_rows] = prep(path+'initial.csv')
     [unrel_methods, unrel_rows] = prep(path+'unrelated.csv')
 
+    # plot initial character as normal
     plot(init_methods, init_rows, ['Easy', 'Medium', 'Hard'])
-    plot(unrel_methods, unrel_rows, ['Easy', 'Medium', 'Hard'])
+
+    # subplot unrelated character
+    subplot(unrel_methods, unrel_rows, ['Easy', 'Medium', 'Hard'])
 
 
 """
@@ -33,10 +32,13 @@ def prep(path):
     df = pd.read_csv(path)
     header = list(df.columns)
 
+    # transpose to read each row as method
+    df = df.transpose()
+
     lst = []
 
-    for i,r in df.itertuples():
-        lst.append(r)
+    for i,r in df.iterrows():
+        lst.append(r.to_list())
 
     return [header, lst]
 
@@ -56,7 +58,7 @@ def plot(method, data, difficulty):
     for i in range(len(method)):
         groups[method[i]] = data[i]
     
-    fig, ax = plt.subplots(layout='constrained')
+    fig, ax = plt.subplots(layout='constrained', figsize=(7, 6))
 
     # place bars in chart
     for att, mea in groups.items():
@@ -65,14 +67,87 @@ def plot(method, data, difficulty):
         ax.bar_label(bars, padding=3)
         mult += 1
 
-    # tune and format plot    
+    # tune and format plot
     ax.set_xticks(x_axis, difficulty)
-    ax.set_xlabel("Difficulty of Recognition")
-    ax.set_ylabel("Accuracy (%)")
+    ax.set_xlabel('Difficulty of Recognition')
+    ax.set_ylabel('Accuracy (%)')
     ax.set_ylim([0, 1.1])
-    ax.set_title("Recognition Accuracy of Machine Learning Algorithms", y=1.1)
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), fancybox=True, ncol=len(method))
+    ax.set_title('Recognition Accuracy of Machine Learning Algorithms', y=1.1)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=len(method), frameon=False, fontsize=8)
     plt.show()
+
+
+"""
+plot unrel char - unable to split without extra work (i'm lazy)
+"""
+def subplot(method, data, difficulty):
+
+    """
+    split data in half so only one char per difficulty
+    """
+    d1 = []
+    d2 = []
+
+    # for each method, split
+    for i in range(len(data)):
+        t1 = []
+        t2 = []
+
+        # for each difficulty, split
+        for j in range(0, len(data[i]), 2):
+            t1.append(data[i][j])
+            t2.append(data[i][j+1])
+        
+        # append split method to list
+        d1.append(t1)
+        d2.append(t2)
+    
+    # method and data dictionary    
+    g1 = {}
+    g2 = {}
+    for i in range(len(method)):
+        g1[method[i]] = d1[i]
+        g2[method[i]] = d2[i]
+
+    """
+    plot
+    """
+    # prepare plot variables
+    x_axis = np.arange(len(difficulty))
+    width = 0.25
+    mult = 0
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 6), sharex=True)
+
+    # place bars in chart
+    for att, mea in g1.items():
+        offset = width * mult
+        bars = ax1.bar(x_axis + offset, mea, width, label=att)
+        ax1.bar_label(bars, padding=3)
+        mult += 1
+    
+    # reset multiplier for subplot
+    mult = 0
+    for att, mea in g2.items():
+        offset = width * mult
+        bars = ax2.bar(x_axis + offset, mea, width, label=att)
+        ax2.bar_label(bars, padding=3)
+        mult += 1
+
+    """
+    tune and format plot
+    """
+    ax1.set_ylim([0, 1.2])
+    ax2.set_ylim([0, 1.2])
+
+    # full plot - 2nd plot is on bottom
+    ax2.set_xticks(x_axis, difficulty)
+    ax2.set_xlabel('Difficulty of Recognition')
+    ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=len(method), frameon=False, fontsize=8)
+    fig.text(0.01, 0.5, 'Accuracy (%)', va='center', rotation='vertical')
+    fig.suptitle('Recognition Accuracy of Machine Learning Algorithms', fontsize=12)
+    plt.show()
+
 
 
 if __name__ == '__main__':
