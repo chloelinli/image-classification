2b8c47d1c1f23dbcbb6773bd5bcadb8d846ae4ae
chloe <chloe.rushing@outlook.com>
2025-10-17 20:17:59 -0700

    confirm updated convert_images script works! yay!

--- run_files.py
+++ run_files.py
@@ -1,12 +1,7 @@
 import os
 
 # image conversion
-"""
-os.system('python3 projects/art_facial_recognition/final/image_to_csv.py')
-os.system('python3 projects/art_facial_recognition/final/image_to_csv_E.py')
-os.system('python3 projects/art_facial_recognition/final/image_to_csv_M.py')
-os.system('python3 projects/art_facial_recognition/final/image_to_csv_H.py')
-"""
+os.system('python3 convert_images.py')
 
 # eigen - image reconstruction
 """
@@ -17,12 +12,18 @@ os.system('python3 projects/art_facial_recognition/final/results/reconstruct_eig
 image recognition
 """
 # eigen
+"""
 os.system('python3 projects/art_facial_recognition/final/results/recognize_eigen.py')
+"""
 
 # xgboost
+"""
 os.system('python3 projects/art_facial_recognition/final/results/recognize_xgboost.py')
+"""
 
 """
 graphing analysis
 """
+"""
 #os.system('python3 projects/art_facial_recognition/final/graph_accuracy.py')
+"""
