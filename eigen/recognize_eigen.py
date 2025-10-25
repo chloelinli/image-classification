7a05f3200003c08b7fc0f87d85bba5a684e09021
chloe <chloe.rushing@outlook.com>
2025-10-24 17:01:52 -0700

    new check_accuracy func

--- eigen/recognize_eigen.py
+++ eigen/recognize_eigen.py
@@ -40,10 +40,11 @@ def main():
     #accuracy(train, data_k99)
 
     # recognition
-    recog = recognition(test, avg, V, scores)
+    pred = recognition(test, avg, V, scores)
 
-    
     # check accuracy for each character, save values
+    accu = check_accuracy(pred, test_ind)
+    print(accu)
     #[init, unrel] = check_tuple(testE_reco, testM_reco, testH_reco)
 
     # export to csv
@@ -292,45 +293,6 @@ def recognition(test_data, avg, V, scores):
 
 
 """
-this method passes in tuples containg values for comparison
-arguments:
-    testE_reco: easy smallest difference index
-    testM_reco: medium smallest difference index
-    testH_reco: hard smallest difference index
-returns list of counts of actual image matches
-"""
-def check_tuple(testE_reco, testM_reco, testH_reco):
-
-    # initial character
-    e_init = (testE_reco, 0, 30, 0, 10)
-    m_init = (testM_reco, 0, 30, 0, 10)
-    h_init = (testH_reco, 0, 30, 0, 10)
-    init = [e_init, m_init, h_init]
-    init_actual = []
-
-    for i in range(len(init)):
-        init_actual.append(accu(init[i]))
-    
-    # unrelated characters, e/m/h
-    e_1 = (testE_reco, 30, 35, 10, 15)
-    e_2 = (testE_reco, 35, 40, 15, 20)
-
-    m_1 = (testM_reco, 40, 45, 10, 15)
-    m_2 = (testM_reco, 45, 50, 15, 20)
-
-    h_1 = (testH_reco, 50, 55, 10, 15)
-    h_2 = (testH_reco, 55, 60, 15, 20)
-
-    unrelated = [e_1, e_2, m_1, m_2, h_1, h_2]
-    unrel_actual = []
-
-    for j in range(len(unrelated)):
-        unrel_actual.append(accu(unrelated[j]))
-    
-    return [init_actual, unrel_actual]
-
-
-"""
 this method compares the expected and actual results of the
 recognition method
 arguments:
@@ -343,23 +305,18 @@ arguments:
         in_end: ending index for estimated indicies
 returns percentage of actual matches
 """
-def accu(temp):
+def check_accuracy(pred, actual):
 
-    indicies = temp[0]
-    start = temp[1]
-    end = temp[2]
-    in_start = temp[3]
-    in_end = temp[4]
+    correct = 0
 
-    actual = 0
-
-    for i in range(in_start, in_end):
-        if indicies[i] >= start and indicies[i] < end:
-            actual += 1
+    size = len(actual)
     
-    accuracy = actual / len(indicies[in_start:in_end])
+    for i in range(size):
+        if int(actual[i]) == int(pred[i]):
+            correct += 1
+    
 
-    return accuracy
+    return abs(correct-size)/size * 100
 
 
 """
