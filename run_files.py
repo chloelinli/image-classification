15be93cfdeaad5804a3d36c1733b49804b4a7e31
chloe <chloe.rushing@outlook.com>
2025-10-27 20:04:19 -0700

    formatting

--- run_files.py
+++ run_files.py
@@ -9,7 +9,7 @@ print("now running eigenvalue and svd recognition: loading...")
 start = time.perf_counter()
 os.system('python3 eigen/recognize_eigen.py')
 end = time.perf_counter()
-print(f"time elapsed: {end-start} seconds")
+print(f"time elapsed: {end-start:.3f} seconds")
 
 print()
 print("********")
@@ -20,7 +20,7 @@ print("now running xgboost tree recognition: loading...")
 start = time.perf_counter()
 os.system('python3 xgboost/recognize_xgboost.py')
 end = time.perf_counter()
-print(f"time elapsed: {end-start} seconds")
+print(f"time elapsed: {end-start:.3f} seconds")
 
 """
 graphing analysis
