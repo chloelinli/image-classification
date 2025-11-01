4d79b513229986faa63ae999da8150a9880682d1
chloe <chloe.rushing@outlook.com>
2025-10-31 20:59:29 -0700

    copied full data split func from eigen

--- linearsvc/linearsvc.py
+++ linearsvc/linearsvc.py
@@ -69,7 +69,38 @@ def get_labels(size):
             arr.append(6)
 
     return np.array(arr)
+
     
+def train_test_split(X, y, test_size, data_size):
+
+    """
+    splits data array based on test set size and dataset size
+    
+    ### arguments:
+    arr: numpy array of dataset\n
+    test_size: percentage of dataset to create as test set\n
+    data_size: size of dataset
+
+    ### returns:
+    train: training data\n
+    test: test data\n
+    train_ind: indicies of training data\n
+    test_ind: indicies of test data
+    """
+
+    # store mixed up indicies of array and save split sizes
+    indicies = np.random.permutation(data_size)
+    
+    test_p = int(data_size * test_size)
+    train_p = int(data_size - test_p)
+
+    # split indicies based on split sizes then use indicies to split data to return
+    train_ind, test_ind = indicies[:train_p], indicies[train_p:]
+    train_X, test_X = X[train_ind,:], X[test_ind,:]
+    train_y, test_y = y[train_ind], y[test_ind]
+
+    return train_X, test_X, train_y, test_y, train_ind
+
     
 if __name__ == '__main__':
     main()
