3301b299006fdb004a6a1c4a00ffe441f04ffa8c
chloe <chloe.rushing@outlook.com>
2025-10-31 20:45:01 -0700

    start file for linearsvc method

---
+++ linearsvc/linearsvc.py
@@ -0,0 +1,17 @@
+# import statements
+import numpy as np
+import pandas as pd
+
+# global variables
+TEST_SIZE = 0.15
+
+# reproducibility
+np.random.seed(7)
+
+
+def main():
+    print()
+
+    
+if __name__ == '__main__':
+    main()
