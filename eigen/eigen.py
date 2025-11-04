0484530aba0a70d3562ecf041269d86a8c6041dd
chloe <chloe.rushing@outlook.com>
2025-11-03 17:50:32 -0800

    function comment update

--- eigen/eigen.py
+++ eigen/eigen.py
@@ -55,11 +55,8 @@ def main():
 def get_labels():
 
     """
-    creates array of labels corresponding to the character 
+    grabs array of labels corresponding to the character 
     in each image
-    
-    ### arguments:
-    size: length of dataset
 
     ### returns:
     arr: numpy array containing integer labels
