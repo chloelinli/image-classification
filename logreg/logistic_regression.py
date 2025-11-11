d2dbf4da75ebac0665dfbf7dffc9e801e4c08fbc
chloe <chloe.rushing@outlook.com>
2025-11-10 18:08:27 -0800

    copied train test split

--- logreg/logistic_regression.py
+++ logreg/logistic_regression.py
@@ -14,6 +14,7 @@ def main():
     # get full data and split into train test sets
     X = np.genfromtxt('data/converted_dataset.csv', delimiter=',')
     y = get_labels()
+    train_X, test_X, train_y, test_y = train_test_split(X, y, TEST_SIZE, len(X))
 
 
 def get_labels():
@@ -32,6 +33,38 @@ def get_labels():
     labels_lst = labels['label'].tolist()
 
     return np.array(labels_lst)
+
+
+def train_test_split(X, y, test_size, data_size):
+
+    """
+    splits data array based on test set size and dataset size
+    
+    ### arguments:
+    X: numpy array of dataset\n
+    y: dataset label\n
+    test_size: percentage of dataset to create as test set\n
+    data_size: size of dataset
+
+    ### returns:
+    train_X: training data split\n
+    test_X: testing data split\n
+    train_y: training data labels\n
+    test_y: test data actual labels
+    """
+
+    # store mixed up indicies of array and save split sizes
+    indicies = np.random.permutation(data_size)
+    
+    test_p = int(data_size * test_size)
+    train_p = int(data_size - test_p)
+
+    # split indicies based on split sizes then use indicies to split data to return
+    train_ind, test_ind = indicies[:train_p], indicies[train_p:]
+    train_X, test_X = X[train_ind,:], X[test_ind,:]
+    train_y, test_y = y[train_ind], y[test_ind]
+
+    return train_X, test_X, train_y, test_y
 
 
 if __name__ == '__main__':
