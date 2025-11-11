b125c0b6a13c8fe185580eb6f104f54b376fd174
chloe <chloe.rushing@outlook.com>
2025-11-10 19:41:13 -0800

    0% prediction accuracy...

--- logreg/logistic_regression.py
+++ logreg/logistic_regression.py
@@ -25,6 +25,10 @@ def main():
     # compute prediction scores
     test_preds = predict(test_X, weight, bias)
 
+    # check accuracy of predictions
+    accu = check_accuracy(test_preds, test_y)
+    print(f"recognition accuracy: {accu:.2f}%")
+
 
 def get_labels():
 
@@ -112,6 +116,31 @@ def predict(test_X, W, B):
     prob = sigmoid(z)
 
     return (prob >= THRESHOLD).astype(int)
+
+
+def check_accuracy(pred, actual):
+
+    """
+    this function the expected and actual image labels generated 
+    from the predictions tree
+    
+    ### arguments:
+    pred: expected image labels\n
+    actual: actual image labels
+
+    ### returns:
+    error rate of exact classification using formula (measured - given) / given * 100
+    """
+
+    correct = 0
+
+    size = len(actual)
+    
+    for i in range(size):
+        if int(actual[i]) == int(pred[i]):
+            correct += 1
+
+    return 100 - (abs(correct-size)/size * 100)
 
 
 if __name__ == '__main__':
